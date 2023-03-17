5b665f57c61790ba7785531481d5170c9038b580
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-03-17 12:19:27 +0300

    Update admin.py
    added eng

--- one_to_one/users/admin.py
+++ one_to_one/users/admin.py
@@ -5,18 +5,18 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 
-# Определим описание для встраивания в админку
+# Description to insert in the admin interface
 class ProfileInlined(admin.TabularInline):
     model = Profile
     can_delete = False
 
 
-# Определим новый вид UserAdmin
+# Define the UserAdmin class
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInlined,)
 
 
-# Перерегистрируем UserAdmin
-# Это позволит использовать админку с дополнительными полями вместо штатной
+# Register UserAdmin
+# This will allow us to use the admin site with the added fields instead of the default version
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
